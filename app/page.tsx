2803fb868b5ed1108000a7ddff9668a842b5d9c5
Mir Tauhidul Islam <mislam.tauhidul@gmail.com>
2024-10-13 18:16:41 +0600

    added tooltip

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <Image
         src={avatar}
         alt="Profile photo"
-        className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0 hover:scale-105 transition-transform"
+        className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale dark:grayscale-0"
         unoptimized
         width={240}
         height={240}
